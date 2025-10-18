ae72788de68c26e28d6b4d5dd7d4255b27991df8
Patrick Silva <patricsilva4cs@gmail.com>
2025-10-18 11:53:30 -0300

    feat/ updating user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,28 @@
+import uuid
 from ..extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    display_name = db.Column(db.String(120))
-    email = db.Column(db.String(180), unique=True, index=True)
+    uuid = db.Column(db.String(36), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(180), unique=True, index=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def set_password(self, password: str, password_confirm: str):
+        if password != password_confirm:
+            raise ValueError("Passwords do not match")
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
 
     def to_dict(self):
-        return {"id": self.id, "display_name": self.display_name, "email": self.email}
+        return {
+            "uuid": self.uuid,
+            "name": self.name,
+            "email": self.email,
+        }
